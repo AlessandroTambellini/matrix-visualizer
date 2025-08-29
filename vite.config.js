f41dcda5f448ede11cf6c67fa62da6812deb4d1f
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-08-29 20:56:19 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    root: './src',
-    base: '/3d-matrix-visualizer/',
-    build: {
-        outDir: '../dist',  
-        emptyOutDir: true,
-    },
+    base: "/3d-matrix-visualizer/"
 });
