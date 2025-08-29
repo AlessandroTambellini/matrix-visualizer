5a402347d5113484a0afebd410f0bfdb2be78391
Alessandro Tambellini <alessandrotambellini03@gmail.com>
2025-08-29 20:45:27 +0200

    fix vite config (5)

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
     root: './src',
+    base: './',
     build: {
         outDir: '../dist',  
-        emptyOutDir: true   
+        emptyOutDir: true,
     },
 });
